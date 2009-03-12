ccc373e0a990a08e638813fe6b1ff776b2367bcd
haach76 <haach76@bdf1b784-0ec1-11de-a4b8-0f3aab1a9b0e>
2009-03-12 04:56:12 +0000

    checking in the initial files

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "argproc.h"
+#include "HRImage.hpp"
+
+int main(int argc, char *argv[])
+{
+
+    Parameters my_parms;
+
+    //set default parameters
+    set_default_param(&my_parms);
+
+    //Process command-line arguments
+    argument_processing(argc, argv, &my_parms);
+
+   printf("directory is %s \n",my_parms.dirName.c_str());
+HRImageSet imSet(my_parms.dirName);
+//imSet.showOneByOne();
+   return 0;
+}
+
+
+
+
+
+
