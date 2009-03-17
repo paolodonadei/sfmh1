494e1b513392c118a33308b910fdace4890caadf
haach76 <haach76@bdf1b784-0ec1-11de-a4b8-0f3aab1a9b0e>
2009-03-17 21:54:10 +0000

    adding some files

--- HRprimitives.cpp
+++ HRprimitives.cpp
@@ -1,10 +1,17 @@
 #include "HRprimitives.h"
 
-typedef boost::shared_ptr<CvPoint2D32f> HR2DFeature;
 
-HRFeatures
+
+
+HRFeature::HRFeature()
 {
-
-   vector<HR2DFeature> HR2DVector;
-
+    valid=false;
+    pvi=0;//never to be used
+}
+HRFeature::HRFeature(int x,int y)
+{
+    location.x=x;
+    location.y=y;
+    valid=true;
+    pvi=0.5;//equally likely as any other feature
 }
---
+++ general.h
@@ -0,0 +1,30 @@
+#ifndef GENERAL_HPP_INCLUDED
+#define GENERAL_HPP_INCLUDED
+
+
+ #include <iostream>
+ #include <sstream>
+ #include <string>
+ #include <typeinfo>
+ #include <stdexcept>
+
+ class BadConversion : public std::runtime_error {
+ public:
+   BadConversion(const std::string& s)
+     : std::runtime_error(s)
+     { }
+ };
+
+ template<typename T>
+ inline std::string stringify(const T& x)
+ {
+   std::ostringstream o;
+   if (!(o << x))
+     throw BadConversion(std::string("stringify(")
+                         + typeid(x).name() + ")");
+   return o.str();
+ }
+
+
+
+#endif //HRPRIMITIVES_HPP_INCLUDED
