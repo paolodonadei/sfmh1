2b9631b515d8e0deb210b2191e02f5d127a1b1ea
haach76 <haach76@bdf1b784-0ec1-11de-a4b8-0f3aab1a9b0e>
2009-07-15 15:54:00 +0000

    misc

---
+++ trunk/utils/fundutils/skewsymmetrify3X3.m
@@ -0,0 +1,14 @@
+function skew = skewsymmetrify3X3(RHS)
+
+skew=zeros(3,3);
+
+skew(0+1,1+1)= -RHS(2+1,0+1);
+skew(0+1,2+1)= RHS(1+1,0+1);
+skew(1+1,0+1)= RHS(2+1,0+1);
+skew(1+1,2+1)= -RHS(0+1,0+1);
+skew(2+1,0+1)= -RHS(1+1,0+1);
+skew(2+1,1+1)= RHS(0+1,0+1);
+
+
+
+endfunction
