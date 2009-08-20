4ffd0a6c1798d7712f816f6a10055d12f426b6a7
haach76 <haach76@bdf1b784-0ec1-11de-a4b8-0f3aab1a9b0e>
2009-08-20 22:37:12 +0000

    same as last commit

--- utils/selfcalib/focallength.h
+++ utils/selfcalib/focallength.h
@@ -19,11 +19,13 @@
 #include <sstream>
 
 
-
+enum SELFCALIBMETHOD{STRUM, POLLEFEY, HARTLEY};
 
 
 using namespace std;
 
+int HRSelfCalibtwoFrame(const CvMat* pF,int width1, int height1, int width2, int height2,CvMat* K1,CvMat* K2,SELFCALIBMETHOD method);
+int  estimateFocalLengthsPollefey(const CvMat* pF, int width1, int height1,int width2, int height2,double& foc1,double& foc2);
 int  estimateFocalLengthStrum(const CvMat* pF,int width, int height, double& foc);
 
 int createPseudoFundMatrix(const CvMat* pF,CvMat* pG,int width, int height);
