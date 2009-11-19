2c74fd3c057eeab3b0d5ee7b03e3c5e105b2e549
haach76 <haach76@bdf1b784-0ec1-11de-a4b8-0f3aab1a9b0e>
2009-11-19 19:53:57 +0000

    added files for generating plots

--- selfcalibmatlab/generateF.m
+++ selfcalibmatlab/generateF.m
@@ -1,7 +1,7 @@
-function [ F ] = generateF( fdiff, skew, ar,centerdeviation )
+function [ F, ks ] = generateF( fdiff, skew, ar,centerdeviation )
 
 
- ps  = generatePs( fdiff, skew, ar,centerdeviation, 2,1 );
+[ ps ,myks ] = generatePs( fdiff, skew, ar,centerdeviation, 2,1 );
 
 P1=ps{1,1};
 P2=ps{1,2};
@@ -9,6 +9,7 @@ P2=ps{1,2};
 
 F = vgg_F_from_P(P1, P2);
 
+ks=myks;
 
 
 
