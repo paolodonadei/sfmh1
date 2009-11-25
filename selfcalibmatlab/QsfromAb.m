2d7d2d6871e49cea0312545d38562abf89e766f6
haach76 <haach76@bdf1b784-0ec1-11de-a4b8-0f3aab1a9b0e>
2009-11-25 22:18:52 +0000

    misc

--- selfcalibmatlab/QsfromAb.m
+++ selfcalibmatlab/QsfromAb.m
@@ -12,9 +12,42 @@ biszeros=1;
 
 [m n]=size(b);
 
+for i=1:m
+    if(b(i,1)>0)
+        biszeros=0;
+        break;
+    end
+end
+
+
+[U,S,V] = svd(A);
+
+if(biszeros==1)
+    
+    
+    Q1=myFormatvectoQ(V(:,10));
+    Q2=myFormatvectoQ(V(:,9));
+    
+else
+    y=zeros(10,1);
+    bp=(U')*b;
+    r=rank(A);
+    
+    for i=1:r
+        y(i,1)=bp(i,1)/S(i,i);  
+    end
+    X=V*y;
+    Q1=X; zaza i waz here xxxx zzzzz
+    Q2=myFormatvectoQ(V(:,9));
+end
 
 
 
+
+end
+
+
+disp([ 'last svd is ' num2str(S(10,10)) ' and the condition number is (it should be near 1) ' num2str(cond(A)) ' and the rank is ' num2str(rank(A))] );
 
 
 
