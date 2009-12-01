be0890063d5325e20383623ad58061c015e58b88
haach76 <haach76@bdf1b784-0ec1-11de-a4b8-0f3aab1a9b0e>
2009-12-01 23:32:10 +0000

    stuff

--- selfcalibmatlab/findNormalizingK.m
+++ selfcalibmatlab/findNormalizingK.m
@@ -3,7 +3,7 @@ function [ K_norm, K_norm_inverse ] = findNormalizingK(w,h)
 
 %create the normalizing matrix
 K_norm=zeros(3,3);
-K_norm(1,1)=  w+h;
+K_norm(1,1)=  w+h;  
 K_norm(2,2)=  w+h;
 K_norm(3,3)=  1;
 K_norm(1,3)=  w/2;
