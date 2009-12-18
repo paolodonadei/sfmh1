0a34db6b89401811b6382f8e6fc13fa1642e6a62
haach76 <haach76@bdf1b784-0ec1-11de-a4b8-0f3aab1a9b0e>
2009-12-18 04:58:55 +0000

    stuff

--- selfcalibmatlab/EvalErrorParams1.m
+++ selfcalibmatlab/EvalErrorParams1.m
@@ -1,5 +1,5 @@
 function [myerr ] = EvalErrorParams1(F,f1,f2,x1,y1,x2,y2 )
-
+myerr=0;
 K_norm1=zeros(3,3);
 K_norm2=zeros(3,3);
 
@@ -20,7 +20,7 @@ K_norm2(3,2)= y2;
 
 
 G=(K_norm2)*F*K_norm1;
-
+G=G/G(3,3);
 
 S = svd(G);
 
@@ -28,7 +28,10 @@ S = svd(G);
 
 myerr =abs(S(1,1)-S(2,1))+abs(S(3,1));
 
-%RR=2*G*(G')*G- trace(G*(G'))*G;
+RR=2*G*(G')*G- trace(G*(G'))*G;
+%RR
+myerr=myerr+sum(sum(abs(RR)));
 
+%myerr
 end
 
--- selfcalibmatlab/HartleySelfHoumansnake.m
+++ selfcalibmatlab/HartleySelfHoumansnake.m
@@ -1,4 +1,4 @@
-function [res , err ] = HartleySelfHoumansnake( F,w,h )
+function [x] = HartleySelfHoumansnake( F,w,h )
 
 focalstep=1;
 xystep=1;
@@ -21,14 +21,20 @@ bestx2=w/2;
 besty2=h/2;
 
 
+xhlimit=400;
+xllimit=200;
+yhlimit=400;
+yllimit=200;
+fhlimit=1500;
+fllimit=300;
 
 count=1;
-
-while count<1000
-    count=count+1;
+minerror=10000000;
+while count==1
     
+    count=0;
     curerr=10000;
-    minerror=100;
+    
     for x1vals=(x1-xystep):xystep:(x1+xystep)
         for y1vals=(y1-xystep):xystep:(y1+xystep)
             for x2vals=(x2-xystep):xystep:(x2+xystep)
@@ -39,6 +45,8 @@ while count<1000
                             curerr=  EvalErrorParams1(F,f1vals,f2vals,x1vals,y1vals,x2vals,y2vals );
                             
                             if(curerr<minerror)
+                                % curerr
+                                count=1;
                                 minerror=curerr;
                                 bestf1=f1vals;
                                 bestf2=f2vals;
@@ -62,15 +70,39 @@ while count<1000
     x2 = bestx2;
     y2 = besty2;
     
-    disp(['f1=' num2str(f1) '  f2=' num2str(f2) '  x1=' num2str(x1) '  y1=' num2str(y1) '  x2=' num2str(x2) '  y2=' num2str(y2)  ' and count is ' num2str(count)]);
+    disp(['f1=' num2str(f1) '  f2=' num2str(f2) '  x1=' num2str(x1) '  y1=' num2str(y1) '  x2=' num2str(x2) '  y2=' num2str(y2)  ' and minerror is ' num2str(minerror)]);
+    
+    if(x1<xllimit || x1>xhlimit)
+        x1=(randn()*100 ) +(w/2);
+    end
+    
+    
+    if(x2<xllimit || x2>xhlimit)
+        x2=(randn()*100 ) +(w/2);
+    end
+    
+    if(y1<yllimit || y1>yhlimit)
+        y1=(randn()*100 ) +(h/2);
+    end
+    
+    
+    if(y2<yllimit || y2>yhlimit)
+        y2=(randn()*100 ) +(h/2);
+    end
+    
+    if(f1<fllimit || f1>fhlimit)
+        f1=(randn()*500 ) +(h+w);
+    end
+    
+    
+    if(f2<fllimit || f2>fhlimit)
+        f2=(randn()*500 ) +(h+w);
+    end
     
 end
 
-
+count
+minerror
+x=[f1 f2];
 
 end
-
-
-
-
-
