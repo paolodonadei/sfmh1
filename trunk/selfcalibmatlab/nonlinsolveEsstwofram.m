e95ce800f56b172a6278ecedbb92843dd4587e64
haach76 <haach76@bdf1b784-0ec1-11de-a4b8-0f3aab1a9b0e>
2009-12-31 18:37:49 +0000

    Stuff

--- trunk/selfcalibmatlab/nonlinsolveEsstwofram.m
+++ trunk/selfcalibmatlab/nonlinsolveEsstwofram.m
@@ -28,11 +28,13 @@ fvari=50;
 xvari=50;
 yvari=50;
 
+%f = @(x)computerEssentialErrorSquared(x,TF); %squared
 f = @(x)computerEssentialError(x,TF);
 
 ffinals=zeros(numtries,1);
 xfinals=zeros(numtries,1);
 yfinals=zeros(numtries,1);
+scorearray=zeros(numtries,1);
 
 bestf=0;
 bestx=0;
@@ -46,16 +48,17 @@ optionslsqnonlin  =optimset('Display','off','Jacobian','on');
 
 
 for i=1:numtries
-    x0=[ (((rand()-0.5)*2)*fvari)+finit ; (((rand()-0.5)*2)*xvari)+xinit ; (((rand()-0.5)*2)*yvari)+yinit ];
+    x0=[ (randn()*fvari)+finit ; (randn()*xvari)+xinit ; (randn()*yvari)+yinit ];
     
     
-     % [x,fval,exitflag,output]  = fsolve(f ,x0,optionsfsolve);
-    [x,resnorm,fval,exitflag] = lsqnonlin(f,x0,[finit-150; 190; 190],[finit+150; 350; 350],optionslsqnonlin);
+     [x,fval,exitflag,output]  = fsolve(f ,x0,optionsfsolve);
+   % [x,resnorm,fval,exitflag] = lsqnonlin(f,x0,[finit-150; 190; 190],[finit+150; 350; 350],optionslsqnonlin);
     ffinals(i,1)=x(1);
     xfinals(i,1)=x(2);
     yfinals(i,1)=x(3);
     
     curscore=sum((fval.^2));
+    scorearray(i,1)=curscore;
     if(curscore<bestscore)
         bestscore=curscore;
         %  disp(['iteration ' num2str(i)]);
@@ -71,7 +74,51 @@ end
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+sizearray=scorearray;
+scoremedian=median(scorearray);
+for i=1:numtries
+    if(sizearray(i,1)>scoremedian)
+        sizearray(i,1)=1;
+    else
+    sizearray(i,1)=ceil(10+(sizearray(i,1)*(-9/scoremedian)));
+    end
+end
 
+
+
+scatter3(xfinals,yfinals,ffinals, sizearray , 'r')
+title(['all the local minima points scatter plot']);
+xlabel('x coordinates of optical centers');
+ylabel('y coordinates of optical centers');
+zlabel('value of focal length ');
+figure
+
+plot3(xfinals,yfinals,ffinals,'r.','MarkerSize',12)
+title(['all the local minima points']);
+xlabel('x coordinates of optical centers');
+ylabel('y coordinates of optical centers');
+zlabel('value of focal length ');
+figure
+%
+% figure
+% X=[ ffinals  xfinals   yfinals];
+% [idx,ctrs] = kmeans(X,5,'Replicates',5);
+%
+% plot3(X(idx==1,1),X(idx==1,2),X(idx==1,3),'r.','MarkerSize',12)
+% hold
+%
+% plot3(X(idx==2,1),X(idx==2,2),X(idx==2,3),'b.','MarkerSize',12)
+% plot3(X(idx==3,1),X(idx==3,2),X(idx==3,3),'g.','MarkerSize',12)
+% plot3(X(idx==4,1),X(idx==4,2),X(idx==4,3),'y.','MarkerSize',12)
+% plot3(X(idx==5,1),X(idx==5,2),X(idx==5,3),'r.','MarkerSize',12)
+%
+% plot(ctrs(:,1),ctrs(:,2),'kx','MarkerSize',12,'LineWidth',2)
+% plot(ctrs(:,1),ctrs(:,2),'ko','MarkerSize',12,'LineWidth',2)
+% plot(ctrs(:,1),ctrs(:,2),'ko','MarkerSize',12,'LineWidth',2)
+% plot(ctrs(:,1),ctrs(:,2),'ko','MarkerSize',12,'LineWidth',2)
+% plot(ctrs(:,1),ctrs(:,2),'ko','MarkerSize',12,'LineWidth',2)
+
+% legend('Cluster 1','Cluster 2','Cluster 3','Cluster 4','Cluster 5','Centroids','Location','NW')
 
 
 
