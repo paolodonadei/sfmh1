31a46c2b01f16abaaef6d9ec3e09083399640e0a
haach76 <haach76@bdf1b784-0ec1-11de-a4b8-0f3aab1a9b0e>
2010-01-01 01:44:06 +0000

    shape function

--- trunk/selfcalibmatlab/nonlinsolveEsstwofram.m
+++ trunk/selfcalibmatlab/nonlinsolveEsstwofram.m
@@ -22,7 +22,7 @@ end
 
 xinit=256;
 yinit=256;
-numtries=100;
+numtries=200;
 
 fvari=50;
 xvari=50;
@@ -39,7 +39,7 @@ scorearray=zeros(numtries,1);
 bestf=0;
 bestx=0;
 besty=0;
-bestscore=1000;
+bestscore=2000;
 curscore=0;
 
 optionsfsolve  =optimset('Display','off','Jacobian','on','Algorithm','levenberg-marquardt','TolFun',1e-6,'TolX',1e-6);
@@ -51,21 +51,28 @@ for i=1:numtries
     x0=[ (randn()*fvari)+finit ; (randn()*xvari)+xinit ; (randn()*yvari)+yinit ];
     
     
-     [x,fval,exitflag,output]  = fsolve(f ,x0,optionsfsolve);
-   % [x,resnorm,fval,exitflag] = lsqnonlin(f,x0,[finit-150; 190; 190],[finit+150; 350; 350],optionslsqnonlin);
+    [x,fval,exitflag,output]  = fsolve(f ,x0,optionsfsolve);
+    % [x,resnorm,fval,exitflag] = lsqnonlin(f,x0,[finit-150; 190; 190],[finit+150; 350; 350],optionslsqnonlin);
     ffinals(i,1)=x(1);
     xfinals(i,1)=x(2);
     yfinals(i,1)=x(3);
     
-    curscore=sum((fval.^2));
-    scorearray(i,1)=curscore;
+    curscore=sum(abs(fval));
+    
+     [svScore, detScore, EssScore ] = EvalErrorParams1(TF,x(1),x(1),x(2),x(3),x(2),x(3) );
+%     curscore=detScore;   
+    
+       
+   
+       
+       scorearray(i,1)=curscore;
     if(curscore<bestscore)
         bestscore=curscore;
         %  disp(['iteration ' num2str(i)]);
         bestf=x(1);
         bestx=x(2);
         besty=x(3);
-        disp(['iteration ' num2str(i) ' best f is ' num2str(bestf) ' and best x = ' num2str(bestx) ' and best y is ' num2str(besty) ' and score was ' num2str(curscore)]);
+        disp(['iteration ' num2str(i) ' best f is ' num2str(bestf) ' and best x = ' num2str(bestx) ' and best y is ' num2str(besty) ' and score was ' num2str(curscore) ' det score was ' num2str(detScore) ' SV score was ' num2str(svScore) ' and ess score was ' num2str(EssScore)]);
         
         
     end
@@ -80,10 +87,24 @@ for i=1:numtries
     if(sizearray(i,1)>scoremedian)
         sizearray(i,1)=1;
     else
-    sizearray(i,1)=ceil(10+(sizearray(i,1)*(-9/scoremedian)));
+        sizearray(i,1)=ceil(10+(sizearray(i,1)*(-9/scoremedian)));
     end
 end
 
+
+scatter(ffinals, scorearray);
+title(['focal lengths versus energy']);
+xlabel('focal length');
+ylabel('value of the energy function ');
+
+figure;
+
+scatter(ffinals, ln(scorearray));
+title(['log scale focal lengths versus energy']);
+xlabel('focal length');
+ylabel('value of the energy function ');
+
+figure;
 
 
 scatter3(xfinals,yfinals,ffinals, sizearray , 'r')
@@ -99,27 +120,31 @@ xlabel('x coordinates of optical centers');
 ylabel('y coordinates of optical centers');
 zlabel('value of focal length ');
 figure
-%
-% figure
-% X=[ ffinals  xfinals   yfinals];
-% [idx,ctrs] = kmeans(X,5,'Replicates',5);
-%
-% plot3(X(idx==1,1),X(idx==1,2),X(idx==1,3),'r.','MarkerSize',12)
-% hold
-%
-% plot3(X(idx==2,1),X(idx==2,2),X(idx==2,3),'b.','MarkerSize',12)
-% plot3(X(idx==3,1),X(idx==3,2),X(idx==3,3),'g.','MarkerSize',12)
-% plot3(X(idx==4,1),X(idx==4,2),X(idx==4,3),'y.','MarkerSize',12)
-% plot3(X(idx==5,1),X(idx==5,2),X(idx==5,3),'r.','MarkerSize',12)
-%
-% plot(ctrs(:,1),ctrs(:,2),'kx','MarkerSize',12,'LineWidth',2)
-% plot(ctrs(:,1),ctrs(:,2),'ko','MarkerSize',12,'LineWidth',2)
-% plot(ctrs(:,1),ctrs(:,2),'ko','MarkerSize',12,'LineWidth',2)
-% plot(ctrs(:,1),ctrs(:,2),'ko','MarkerSize',12,'LineWidth',2)
-% plot(ctrs(:,1),ctrs(:,2),'ko','MarkerSize',12,'LineWidth',2)
 
-% legend('Cluster 1','Cluster 2','Cluster 3','Cluster 4','Cluster 5','Centroids','Location','NW')
+numclusts=10;
+X=[ ffinals  xfinals   yfinals];
+[idx,ctrs] = kmeans(X,numclusts,'Replicates',5);
 
+scatter3(X(:,1),X(:,2),X(:,3),100,idx,'filled')
+
+maxnumclusts=0;
+maxnumclustsF=0;
+maxnumclustsX=0;
+maxnumclustsY=0;
+
+for i=1:numclusts
+    cursize=sum(idx==i);
+    
+    if(cursize>maxnumclusts)
+        maxnumclusts=cursize;
+        maxnumclustsF=ctrs(i,1);
+        maxnumclustsX=ctrs(i,2);
+        maxnumclustsY=ctrs(i,3);
+    end
+end
+
+
+figure
 
 
 hist(ffinals,numtries/2);
@@ -137,6 +162,8 @@ display(['mean of f was ' num2str(mean(ffinals)) ' median of f was ' num2str(med
 display(['mean of x was ' num2str(mean(xfinals)) ' median of x was ' num2str(median(xfinals)) ' variance of x was ' num2str(var(xfinals))]);
 display(['mean of y was ' num2str(mean(yfinals)) ' median of y was ' num2str(median(yfinals)) ' variance of y was ' num2str(var(yfinals))]);
 display(['best f is ' num2str(bestf) ' and best x is ' num2str(bestx) ' and best y is ' num2str(besty)]);
+display([' the highest cluster number was ' num2str(maxnumclusts) ' whose f is  ' num2str(maxnumclustsF) ' X is  ' num2str(maxnumclustsX) ' Y is  ' num2str( maxnumclustsY)  ]);
+
 
 %
 %
