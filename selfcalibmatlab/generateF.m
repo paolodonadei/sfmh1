c50950a4d68e29ded9eff1aecb5246272eb41d70
haach76 <haach76@bdf1b784-0ec1-11de-a4b8-0f3aab1a9b0e>
2010-01-05 03:54:05 +0000

    implemented the N frame method

--- selfcalibmatlab/generateF.m
+++ selfcalibmatlab/generateF.m
@@ -1,13 +1,34 @@
-function [ F, ks,ps ] = generateF( fdiff, skew, ar,centerdeviation,silent )
+function [ F, ks,ps ] = generateF( fdiff, skew, ar,centerdeviation,silent,number )
 
 
-[ ps ,myks ] = generatePs( fdiff, skew, ar,centerdeviation, 2,1,silent );
+[ ps ,myks ] = generatePs( fdiff, skew, ar,centerdeviation, number,1,silent );
 
 P1=ps{1,1};
 P2=ps{1,2};
 
 
-F = vgg_F_from_P(P1, P2);
+if(number==1)
+    disp(['error']);
+    F=zeros(3);
+elseif(number==2)
+    F = vgg_F_from_P(P1, P2);
+else
+    
+    count=0;
+    for i=1:number
+        for j=1:i
+            if(i~=j)
+                count=count+1;
+                F{count}=vgg_F_from_P(ps{1,i},ps{1,j});
+            end
+        end
+    end
+    
+    
+end
+
+
+
 
 ks=myks;
 
