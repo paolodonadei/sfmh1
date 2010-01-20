5b8783c6bfcd2413118ba2820c78164e41089f78
haach76 <haach76@bdf1b784-0ec1-11de-a4b8-0f3aab1a9b0e>
2010-01-20 00:46:10 +0000

    stuff for the sampling consensus

--- trunk/selfcalibmatlab/S2nonlinsolveEsstwofram.m
+++ trunk/selfcalibmatlab/S2nonlinsolveEsstwofram.m
@@ -24,56 +24,56 @@ yfinals=zeros(numtries,sizeFs);
 scorearray=zeros(numtries,sizeFs);
 
 
-for q=1:n
-    
+for q=1:sizeFs
+
     x = PeterSturmSelf( TF{q},w,h);
-    
+
     if(x(1,1)>200 && x(1,1)<1600)
         finit=x(1,1);
     else
         finit=w+h;
     end
-    
-    
+
+
     xinit=w/2;
     yinit=h/2;
-    
-    
+
+
     fvari=50;
     xvari=50;
     yvari=50;
-    
-    
-    
+
+
+
     bestf=0;
     bestx=0;
     besty=0;
     bestscore=1000000000000;
     curscore=0;
     f = @(x)computerEssentialErrorSVD(x,TF{q});
-    optionsfsolve  =optimset('Display','off','Jacobian','off','Algorithm','levenberg-marquardt','TolFun',1e-6,'TolX',1e-6);
-    
-    
-    
-    
+    optionsfsolve  =optimset('Display','off','Jacobian','off','NonlEqnAlgorithm','lm','TolFun',1e-6,'TolX',1e-6);
+
+
+
+
     for i=1:numtries
         x0=[ (randn()*fvari)+finit  (randn()*xvari)+xinit  (randn()*yvari)+yinit ];
-        
-        
+
+
         [x,fval,exitflag,output]  = fsolve(f ,x0,optionsfsolve);
-        
+
         ffinals(i,q)=x(1);
         xfinals(i,q)=x(2);
         yfinals(i,q)=x(3);
-        
+
         curscore=sum(abs(fval));
-        
+
         [svScore, detScore, EssScore, EssScoreIA ]= EvalErrorParams1(TF{q},x(1),x(1),x(2),x(3),x(2),x(3) );
         curscore=detScore;
-        
+
         % disp(['iteration ' num2str(i) ' started from f= ' num2str(x0(1,1)) ' x= ' num2str(x0(1,2)) ' and y= ' num2str(x0(1,3))]);
-        disp(['fund matrix: ' num2str(q) ' iteration ' num2str(i) ' best f is ' num2str(x(1)) ' and best x = ' num2str(x(2)) ' and best y is ' num2str(x(3)) ' and score was ' num2str(curscore) ' det score was ' num2str(detScore) ' SV score was ' num2str(svScore) ' and ess score was ' num2str(EssScore) ' IA score is ' num2str( EssScoreIA)]);
-        
+        %disp(['fund matrix: ' num2str(q) ' iteration ' num2str(i) ' best f is ' num2str(x(1)) ' and best x = ' num2str(x(2)) ' and best y is ' num2str(x(3)) ' and score was ' num2str(curscore) ' det score was ' num2str(detScore) ' SV score was ' num2str(svScore) ' and ess score was ' num2str(EssScore) ' IA score is ' num2str( EssScoreIA)]);
+
         scorearray(i,q)=curscore;
         if(curscore<bestscore && imag(x(1))==0 && x(1)>200 && x(1)<1600 )
             bestscore=curscore;
@@ -82,18 +82,18 @@ for q=1:n
             bestx=x(2);
             besty=x(3);
             %   x,resnorm,fval,exitflag
-            disp(['fund matrix: ' num2str(q) '**BEST: iteration ' num2str(i) ' best f is ' num2str(x(1)) ' and best x = ' num2str(x(2)) ' and best y is ' num2str(x(3)) ' and score was ' num2str(curscore) ' det score was ' num2str(detScore) ' SV score was ' num2str(svScore) ' and ess score was ' num2str(EssScore) ' IA score is ' num2str( EssScoreIA)]);
-            
+           % disp(['fund matrix: ' num2str(q) '**BEST: iteration ' num2str(i) ' best f is ' num2str(x(1)) ' and best x = ' num2str(x(2)) ' and best y is ' num2str(x(3)) ' and score was ' num2str(curscore) ' det score was ' num2str(detScore) ' SV score was ' num2str(svScore) ' and ess score was ' num2str(EssScore) ' IA score is ' num2str( EssScoreIA)]);
+
         end
     end
-    
+
 end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-for q=1:n
+for q=1:sizeFs
     sizearray=scorearray;
     scoremedian(q)=median(scorearray(:,q));
     for i=1:numtries
@@ -112,13 +112,13 @@ title(['focal lengths versus energy']);
 xlabel('focal length');
 ylabel('value of the energy function ');
 
-for q=1:n
+for q=1:sizeFs
     scatter(ffinals(:,q), scorearray(:,q));
 end
 hold
 
 %%%%%%%%%%%%%%%%%%%%%
-
+styles={'r' ,'g', 'b', 'y', 'r' ,'c'};
 figure
 hold
 title(['all the local minima points scatter plot']);
@@ -126,14 +126,14 @@ xlabel('x coordinates of optical centers');
 ylabel('y coordinates of optical centers');
 zlabel('value of focal length ');
 
-for q=1:n
-    
-    scatter3(xfinals(:,q),yfinals(:,q),ffinals(:,q), 3 , 'r')
+for q=1:sizeFs
+
+    scatter3(xfinals(:,q),yfinals(:,q),ffinals(:,q), 3 ,styles{mod(q,6)+1})
 end
 hold
 
 %%%%%%%%%%%%%%%%%%%%%
-styles={'-.or' ,'-.xg', '-.+b', '-.*y', '-.vr' ,'-..c'};
+
 figure
 hold
 
@@ -142,8 +142,8 @@ xlabel('x coordinates of optical centers');
 ylabel('y coordinates of optical centers');
 zlabel('value of focal length ');
 
-for q=1:n
-plot3(xfinals(:,q),yfinals(:,q),ffinals(:,q),styles{mod(q,6)+1},'MarkerSize',12)
+for q=1:sizeFs
+    plot3(xfinals(:,q),yfinals(:,q),ffinals(:,q),styles{mod(q,6)+1},'MarkerSize',12)
 end
 hold
 
@@ -189,17 +189,17 @@ hold
 %
 % figure
 %
-
-hist3(ffinals,numtries/2);
-title(['focal length']);
-figure
-
-hist3(xfinals,numtries/2);
-title(['xcomponent of camera center']);
-figure
-
-hist3(yfinals,numtries/2);
-title(['ycomponent of camera center']);
+% %
+% hist(ffinals,numtries/2);
+% title(['focal length']);
+% figure
+%
+% hist(xfinals,numtries/2);
+% title(['xcomponent of camera center']);
+% figure
+%
+% hist(yfinals,numtries/2);
+% title(['ycomponent of camera center']);
 %
 % display(['mean of f was ' num2str(mean(ffinals)) ' median of f was ' num2str(median(ffinals)) ' variance of f was ' num2str(var(ffinals))]);
 % display(['mean of x was ' num2str(mean(xfinals)) ' median of x was ' num2str(median(xfinals)) ' variance of x was ' num2str(var(xfinals))]);
@@ -207,8 +207,41 @@ title(['ycomponent of camera center']);
 % display(['best f is ' num2str(bestf) ' and best x is ' num2str(bestx) ' and best y is ' num2str(besty)]);
 % display([' the highest cluster number was ' num2str(maxnumclusts) ' whose f is  ' num2str(maxnumclustsF) ' X is  ' num2str(maxnumclustsX) ' Y is  ' num2str( maxnumclustsY)  ]);
 %
+fedges=200:5:1600;
+fhists = histc(ffinals,fedges) ;
 
+[m,n]=size(fhists);
+for i=1:m
+    for j=1:n
+        if(fhists(i,j)>sizeFs)
+            fhists(i,j)=sizeFs;
+        end
+    end
+end
+[val,findex]=max(sum( fhists,2));
 
+myf=0;
+myx=0;
+myy=0;
+
+[m,n]=size(ffinals);
+count=0;
+for i=1:m
+    for j=1:n
+        if(ffinals(i,j)>fedges(findex) && ffinals(i,j)<fedges(findex+1))
+            count=count+1;
+            myf=myf+ffinals(i,j);
+            myx=myx+xfinals(i,j);
+            myy=myy+yfinals(i,j);
+        end
+    end
+end
+
+myf=myf/count;
+myx=myx/count;
+myy=myy/count;
+
+disp(['best found f is ' num2str(myf) ' and best x is ' num2str(myx) ' and best y is ' num2str(myy)]);
 %toc
 if(bestf>200 && bestf<1600 && imag(bestf)==0)
     bestf=bestf;
@@ -219,6 +252,9 @@ fcl=[ bestf bestf];
 xcen=bestx;
 ycen=besty;
 centerloc=[xcen ycen];
+comments:
 
+use the camera center to generate consensus cuz it seems more stable
+see how you can extend this to teh case where the two frames are different, thats the interesting case
 end
 
