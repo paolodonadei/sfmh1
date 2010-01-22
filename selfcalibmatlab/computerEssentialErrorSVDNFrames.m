476552cab92a0eba67bfe205f9707a7a040bf3e4
haach76 <haach76@bdf1b784-0ec1-11de-a4b8-0f3aab1a9b0e>
2010-01-22 03:24:46 +0000

    n frame svd based

---
+++ selfcalibmatlab/computerEssentialErrorSVDNFrames.m
@@ -0,0 +1,18 @@
+function [F] = computerEssentialErrorSVDNFrames(x,MYF)
+%this function , given a camera center and a focal length and a fundamental
+%matrix computes the error with respect to a fundamental matrix
+[m,n]=size(MYF);
+
+F=zeros(9,1);
+for i=1:n
+    
+T = computerEssentialErrorSVD(x,MYF{1,i});
+
+F=F+(abs(T));
+end
+
+
+
+
+end
+
