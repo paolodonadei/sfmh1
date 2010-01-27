40e0e458dfac33ba3383d0b8e03cc02ca2339a6b
haach76 <haach76@bdf1b784-0ec1-11de-a4b8-0f3aab1a9b0e>
2010-01-27 05:57:48 +0000

    clutering method has some problems, but works fine for real data

--- selfcalibmatlab/S2nonlinsolveEsstwofram.m
+++ selfcalibmatlab/S2nonlinsolveEsstwofram.m
@@ -4,7 +4,7 @@ function [fcl, centerloc] = S2nonlinsolveEsstwofram(TF,w,h)
 %tic
 
 %TF=TF*10000;
-plotting=0;
+plotting=1;
 maxfocal=2000;
 fcl=[0 0];
 
@@ -27,49 +27,49 @@ scorearray=zeros(numtries,sizeFs);
 sturmfailed=0;
 
 for q=1:sizeFs
-
+    
     x = PeterSturmSelf( TF{q},w,h);
-
+    
     if(x(1,1)>200 && x(1,1)<1600)
         finit=x(1,1);
     else
         sturmfailed=1;
     end
-
-
+    
+    
     xinit=w/2;
     yinit=h/2;
-
-
+    
+    
     fvari=70;
     xvari=70;
     yvari=70;
-
-
-
+    
+    
+    
     bestf=0;
     bestx=0;
     besty=0;
     bestscore=1000000000000;
     curscore=0;
     f = @(x)computerEssentialErrorSVD(x,TF{q});
-    optionsfsolve  =optimset('Display','off','Jacobian','off','NonlEqnAlgorithm','lm','TolFun',1e-6,'TolX',1e-6);
-    %   optionsfsolve    =optimset('Display','off','Jacobian','off','Algorithm','levenberg-marquardt','TolFun',1e-6,'TolX',1e-6);
-
-
-
+    % optionsfsolve  =optimset('Display','off','Jacobian','off','NonlEqnAlgorithm','lm','TolFun',1e-6,'TolX',1e-6);
+    optionsfsolve    =optimset('Display','off','Jacobian','off','Algorithm','levenberg-marquardt','TolFun',1e-6,'TolX',1e-6);
+    
+    
+    
     for i=1:numtries
         if(sturmfailed==0)
             x0=[ (randn()*fvari)+finit  (randn()*xvari)+xinit  (randn()*yvari)+yinit ];
         else
             x0=[ (rand()*(maxfocal))  (randn()*xvari)+xinit  (randn()*yvari)+yinit ];
-         
+            
         end
-
-
-
+        
+        
+        
         [x,fval,exitflag,output]  = fsolve(f ,x0,optionsfsolve);
-
+        
         if(x(1)<0 || x(1)>maxfocal || x(2)<0 || x(2)>w || x(3)<0 || x(3)>h)
             ffinals(i,q)=0;
             xfinals(i,q)=0;
@@ -80,15 +80,15 @@ for q=1:sizeFs
             xfinals(i,q)=x(2);
             yfinals(i,q)=x(3);
         end;
-
+        
         curscore=sum(abs(fval));
-
+        
         [svScore, detScore, EssScore, EssScoreIA ]= EvalErrorParams1(TF{q},x(1),x(1),x(2),x(3),x(2),x(3) );
         curscore=detScore;
-
+        
         % disp(['iteration ' num2str(i) ' started from f= ' num2str(x0(1,1)) ' x= ' num2str(x0(1,2)) ' and y= ' num2str(x0(1,3))]);
         %disp(['fund matrix: ' num2str(q) ' iteration ' num2str(i) ' best f is ' num2str(x(1)) ' and best x = ' num2str(x(2)) ' and best y is ' num2str(x(3)) ' and score was ' num2str(curscore) ' det score was ' num2str(detScore) ' SV score was ' num2str(svScore) ' and ess score was ' num2str(EssScore) ' IA score is ' num2str( EssScoreIA)]);
-
+        
         scorearray(i,q)=curscore;
         if(curscore<bestscore && imag(x(1))==0 && x(1)>200 && x(1)<1600 )
             bestscore=curscore;
@@ -98,10 +98,10 @@ for q=1:sizeFs
             besty=x(3);
             %   x,resnorm,fval,exitflag
             % disp(['fund matrix: ' num2str(q) '**BEST: iteration ' num2str(i) ' best f is ' num2str(x(1)) ' and best x = ' num2str(x(2)) ' and best y is ' num2str(x(3)) ' and score was ' num2str(curscore) ' det score was ' num2str(detScore) ' SV score was ' num2str(svScore) ' and ess score was ' num2str(EssScore) ' IA score is ' num2str( EssScoreIA)]);
-
+            
         end
     end
-
+    
 end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -109,7 +109,7 @@ end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 if(plotting==1)
-
+    
     for q=1:sizeFs
         sizearray=scorearray;
         scoremedian(q)=median(scorearray(:,q));
@@ -121,19 +121,19 @@ if(plotting==1)
             end
         end
     end
-
-
+    
+    
     figure
     hold
     title(['focal lengths versus energy']);
     xlabel('focal length');
     ylabel('value of the energy function ');
-
+    
     for q=1:sizeFs
         scatter(ffinals(:,q), scorearray(:,q));
     end
     hold
-
+    
     %%%%%%%%%%%%%%%%%%%%%
     styles={'r' ,'g', 'b', 'y', 'r' ,'c'};
     figure
@@ -142,20 +142,37 @@ if(plotting==1)
     xlabel('x coordinates of optical centers');
     ylabel('y coordinates of optical centers');
     zlabel('value of focal length ');
-
+    
     for q=1:sizeFs
-
+        
         scatter3(xfinals(:,q),yfinals(:,q),ffinals(:,q), 3 ,styles{mod(q,6)+1})
     end
     hold
-
+    
     %%%%%%%%%%%%%%%%%%%%%
-
+    
     figure
+    
+end
 
+%%%%%%%%%%%
+%creating an index of the memberships
+mcount=1;
+for q=1:sizeFs
+    for i=1:numtries
+        idx_membership(mcount)=q;
+        if(ffinals(i,q)<0 || ffinals(i,q)>maxfocal || xfinals(i,q)<0 || xfinals(i,q)>w || yfinals(i,q)<0 || yfinals(i,q)>h)
+            idx_membership(mcount)=0;
+        else
+            idx_membership(mcount)=q;
+        end
+        
+        mcount=mcount+1;
+    end
 end
 
 
+%%%%%%%%%%5
 numclusts=10;
 X=[ reshape(ffinals,sizeFs*numtries,1)  reshape(xfinals,sizeFs*numtries,1)   reshape(yfinals,sizeFs*numtries,1)];
 [idx,ctrs] = kmeans(X,numclusts,'Replicates',5,'EmptyAction','drop');
@@ -173,9 +190,38 @@ maxnumclustsF=0;
 maxnumclustsX=0;
 maxnumclustsY=0;
 
-for i=1:numclusts
-    cursize=sum(idx==i);
 
+ classscores=zeros(numclusts,sizeFs);
+for i=1:numclusts
+  
+    
+    for j=1:(sizeFs*numtries)
+        if(idx(j)==i && idx_membership(j)~=0)
+            classscores(i,idx_membership(j))=classscores(idx_membership(j)) +1;
+        end
+    end
+    
+    
+    %im capping here, but maybe a smooth functio would be a better idea,
+    %basically the idea here is that we prevent one F from overtaking the
+    %cluster scores, cuz this way one bad F that gets stuck in a local
+    %minima will generate teh maximum consensus, but hopefully there is a
+    %more elegant way of doing this, like a smooth decaying exponential or
+    %something
+    
+    
+    
+    uncomment these bro, and your class scores thing is not working
+%     for q=1:sizeFs
+%         if( classscores(i,q)>ceil(numtries/10))
+%             classscores(i,q)=ceil(numtries/10);
+%         end
+%         
+%     end
+    
+   % cursize=sum(idx==i);
+    cursize=sum( classscores(i,:));
+    
     if(cursize>maxnumclusts)
         maxnumclusts=cursize;
         maxnumclustsF=ctrs(i,1);
@@ -188,16 +234,16 @@ end
 
 %
 if(plotting==1)
-
+    
     figure
     hist(reshape(ffinals,sizeFs*numtries,1),numtries/2);
     title(['focal length']);
     figure
-
+    
     hist(reshape(xfinals,sizeFs*numtries,1),numtries/2);
     title(['xcomponent of camera center']);
     figure
-
+    
     hist(reshape(yfinals,sizeFs*numtries,1),numtries/2);
     title(['ycomponent of camera center']);
     %
@@ -207,7 +253,7 @@ bestf=maxnumclustsF;
 bestx=maxnumclustsX;
 besty=maxnumclustsY;
 
-%display([' the highest cluster number was ' num2str(maxnumclusts) ' whose f is  ' num2str(maxnumclustsF) ' X is  ' num2str(maxnumclustsX) ' Y is  ' num2str( maxnumclustsY)  ]);
+display([' the highest cluster number was ' num2str(maxnumclusts) ' whose f is  ' num2str(maxnumclustsF) ' X is  ' num2str(maxnumclustsX) ' Y is  ' num2str( maxnumclustsY)  ]);
 
 
 %toc
--- selfcalibmatlab/comparecluster.m
+++ selfcalibmatlab/comparecluster.m
@@ -1,31 +1,50 @@
 close all
 clear all
 clc
-P1=load('../../data/euclidean/build2/001.P');
-P2=load('../../data/euclidean/build2/002.P');
-P3=load('../../data/euclidean/build2/003.P');
 
-width= 1024 ;
-height=768  ;
-[K1, R, t] = vgg_KR_from_P(P1);
-[K2, R, t] = vgg_KR_from_P(P2);
-[K3, R, t] = vgg_KR_from_P(P3);
-K1
-K2
-K3
-[F] =PsTOFs({P1 P2 P3});
+% 
+% P1=load('../data/euclidean/build2/001.P');
+% P2=load('../data/euclidean/build2/002.P');
+% P3=load('../data/euclidean/build2/003.P');
+% 
+% width= 1024 ;
+% height=768  ;
+% [K1, R, t] = vgg_KR_from_P(P1);
+% [K2, R, t] = vgg_KR_from_P(P2);
+% [K3, R, t] = vgg_KR_from_P(P3);
+% K1
+% K2
+% K3
+%[F] =PsTOFs({P1 P2 P3});
+% gfocal=1140;
+% gocx=  525;
+% gocy=390  ;
+
+[ F, ks ]  = generateF( 0, 4, 1.05,70,0,3 );
+ks{1}
+ks{2}
+ks{3}
+width=512;
+height=512;
+gfocal=(ks{1}(1,1)+ks{1}(2,2))/2;
+gocx=  ks{1}(1,3);
+gocy=ks{1}(2,3)  ;
+
+
+
 
 count=1;
-gfocal=1140;
-gocx=  525;
-gocy=390  ;
-for i=0:0.5:10
+
+for i=0:0.0005:0.003
     G=F;
-    G{1}=G{1} + ((rand(3,3)-0.5)*i);
-    [fcln, centerlocn] = S2nonlinsolveEssNfram(G,width,height);
-    [fclt, centerloct] = S2nonlinsolveEsstwofram(G,width,height);
-    errnfram(count)=abs(fcln(1)-gfocal);
-    errtwofram(count)=abs(fclt(1)-gfocal);
+    NMAT=((ones(3,3)-0.5)*i);
+    NMAT(3,3)=0;
+    
+    G{1}=G{1} +NMAT ;
+    [fcln, centerlocn] = S2nonlinsolveEssNfram(G,width,height)
+    [fclt, centerloct] = S2nonlinsolveEsstwofram(G,width,height)
+    errnfram(count)=abs(fcln(1)-gfocal)
+    errtwofram(count)=abs(fclt(1)-gfocal)
     errnframOC(count)=sqrt(((centerlocn(1)-gocx)^2)+((centerlocn(2)-gocy)^2));
     errtwoframOC(count)=sqrt(((centerloct(1)-gocx)^2)+((centerloct(2)-gocy)^2));
     count=count+1;
@@ -43,4 +62,4 @@ title('optical center error comparison between N frame method and two frame clus
 legend('N frame', ' two frame clustering');
 
 
-ok the noise is added in a bad way that it fuckes up th results
+%ok the noise is added in a bad way that it fuckes up th results
