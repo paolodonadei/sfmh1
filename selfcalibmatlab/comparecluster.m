c4bc0a398b24698985af675a9773e47d110748b3
haach76 <haach76@bdf1b784-0ec1-11de-a4b8-0f3aab1a9b0e>
2010-02-05 05:42:11 +0000

    changed some stuff about the clustercompare script

--- selfcalibmatlab/comparecluster.m
+++ selfcalibmatlab/comparecluster.m
@@ -23,77 +23,76 @@ clc
 nowtime=num2str(sum(round(100*clock)));
 tic
 
-repeat=5;
+repeat=20;
 
 count=1;
 
-for i=0:0.05:1
+width=512;
+height=512;
 
+for i=0:0.05:0.5
+    
     for k=1:repeat
-        change this to the new F generation
+        %  change this to the new F generation
+        clear G F ks;
         
-        [ F, ks ]  = generateF( 0, 0, 1.00,rand()*80,0,5 );
+        [ F, ks ]  = generateF( 0, 0, 1.00,rand()*60,0,4,i,2 );
         %         ks{1}
         %         ks{2}
         %         ks{3}
         [nn, numfunds]=size(F);
-        fundflip=ceil((rand()*(numfunds)));
         
-        width=512;
-        height=512;
+        
         gfocal=(ks{1,1}(1,1)+ks{1,1}(2,2))/2;
         gocx=  ks{1,1}(1,3);
         gocy=ks{1,1}(2,3)  ;
         G=F;
-        NMAT=((rand(3,3)-0.5)*i*(abs(sum(sum((G{1,fundflip}))))));
-        NMAT(3,3)=0;
-
-        G{1,fundflip}=G{1,fundflip} +NMAT ;
+        
         
         [fcln, centerlocn]      = S2nonlinsolveEssNfram(G,width,height)
         [fclt, centerloct]      = S2nonlinsolveEsstwofram(G,width,height)
         [fcld, centerlocdiag]   = S2nonlinsolveEssNframdiagnostics(G,width,height)
         [fclm, centerlocM]      = S2nonlinsolveEssNframestimator(G,width,height)
         [ sln, centerloc ]      = PeterSturmSelfRobust( G,width,height )
-
+        
         errps(k,count)=abs(sln(1)-gfocal);
         errnfram(k,count)=abs(fcln(1)-gfocal);
         errtwofram(k,count)=abs(fclt(1)-gfocal);
         errdiagn(k,count)=abs(fcld(1)-gfocal);
         errM(k,count)=abs(fclm(1)-gfocal);
-
-
-        sum( errps)
-        sum(errnfram)
-        sum(errtwofram)
-        sum( errdiagn)
-        sum(errM)
-
+        
+        
+        errpssum= sum( errps)
+        errnframsum= sum(errnfram)
+        errtwoframsum= sum(errtwofram)
+        errdiagnsum= sum( errdiagn)
+        errMsum= sum(errM)
+        
         errnframOC(k,count)=sqrt(((centerlocn(1)-gocx)^2)+((centerlocn(2)-gocy)^2));
         errtwoframOC(k,count)=sqrt(((centerloct(1)-gocx)^2)+((centerloct(2)-gocy)^2));
         errdiagOC(k,count)=sqrt(((centerlocdiag(1)-gocx)^2)+((centerlocdiag(2)-gocy)^2));
         errMOC(k,count)=sqrt(((centerlocM(1)-gocx)^2)+((centerlocM(2)-gocy)^2));
-
+        
         disp(['display iteration ' num2str(k+((count-1)*repeat))  ]);
     end
     count=count+1;
-
+    
 end
 
 
-plot([sum(errnfram)' sum(errtwofram)' sum(errps)' sum(errdiagn)' sum(errM)']);
+plot([(errnframsum/repeat)' (errtwoframsum/repeat)' (errpssum/repeat)' (errdiagnsum/repeat)' (errMsum/repeat)']);
 title('focal length error comparison between N frame method and two frame clustering');
 legend('N frame', ' two frame clustering' , 'error peter sturm', 'case deletion', 'M-estimator');
 saveas(gcf,['erroFlength_' nowtime '.png']);
-saveas(gcf,['erroFlength_' nowtime '.eps']);
+saveas(gcf,['erroFlength_' nowtime '.eps'],'epsc');
 figure
-plot([sum(errnframOC)' sum(errtwoframOC)' sum(errdiagOC)'  sum(errMOC)']);
+plot([(sum(errnframOC)/repeat)' (sum(errtwoframOC)/repeat)' (sum(errdiagOC)/repeat)'  (sum(errMOC)/repeat)']);
 title('optical center error comparison between N frame method and two frame clustering');
 legend('N frame', ' two frame clustering', ' case deletion', ' M-Estimator');
 saveas(gcf,['erroOC_' nowtime '.png']);
-saveas(gcf,['erroOC_' nowtime '.eps']);
+saveas(gcf,['erroOC_' nowtime '.eps'],'epsc');
 
-save( ['variables_' nowtime '.m'])
+save( ['variables_' nowtime '.mat'])
 
 toc
 %ok the noise is added in a bad way that it fuckes up th results
