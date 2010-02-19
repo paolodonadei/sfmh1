44c76f10303951e045990cc9039dda97b081f6ee
haach76 <haach76@bdf1b784-0ec1-11de-a4b8-0f3aab1a9b0e>
2010-02-19 22:34:53 +0000

    misc

--- trunk/selfcalibmatlab/generateSelfPlot.m
+++ trunk/selfcalibmatlab/generateSelfPlot.m
@@ -111,8 +111,11 @@ for i=1:numPoints
 
     for j=1:repeat
         currIteration=currIteration+1;
-             [ F, ks ] = generateF( fdiff(1,i), skew(1,i), aspect(1,i),centerdev(1,i),1,numPs,n(1,i),b(1,i)   );
-        % [corrs, IMS, P,ks, F] = readCorrsOxford('C:\Documents and Settings\hrast019\Desktop\data\euclidean\wadham', n(1,i), b(1,i));
+        %     [ F, ks ] = generateF( fdiff(1,i), skew(1,i), aspect(1,i),centerdev(1,i),1,numPs,n(1,i),b(1,i)   );
+        
+        [corrs, IMS, P,ks, F] = readCorrsOxford('C:\Documents and Settings\hrast019\Desktop\data\euclidean\wadham', n(1,i), b(1,i));
+       width=1024; height=768;
+        
         disp(['****iteration ' num2str(currIteration) ' out of ' num2str(numTotalIterations) '   AND calling generateF( ' num2str(fdiff(1,i)) ' , ' num2str(skew(1,i)) ' , '  num2str(aspect(1,i)) ' , ' num2str(centerdev(1,i)) ' , 1 , ' num2str(numPs) ' , ' num2str(n(1,i)) ' , ' num2str(b(1,i)) ')'] );
 
         for k=1:numalgs
