fb89e1431e409d6a5e6f67574206b9b38940fd10
haach76 <haach76@bdf1b784-0ec1-11de-a4b8-0f3aab1a9b0e>
2010-03-04 20:47:06 +0000

    misc

--- selfcalibmatlab/findSVDthreshold.m
+++ selfcalibmatlab/findSVDthreshold.m
@@ -1,17 +1,19 @@
-function [threshold]= findSVDthreshold(TF,w,h)
+function [threshold]= findSVDthreshold(TF,w,h,x)
 
-x=[w w/2 h/2 w];
+if (nargin == 3)
+    x=[w w/2 h/2 w];
+end
 
 [m,numFs]=size(TF);
 
 for j=1:numFs
-obj(j,1)=computerEssentialErrorSVDNFramesWeighted(x,{TF{j}});    
+    obj(j,1)=computerEssentialErrorSVDNFramesWeighted(x,{TF{j}});
 end
 
 %hist(obj,100)
 
-curMedian=median(obj);
-MADN=1.96*(median(abs(obj-curMedian))/0.6745);
+% curMedian=median(obj);
+% MADN=1.96*(median(abs(obj-curMedian))/0.6745);
 
 MADN=1.96*prctile(obj,68.27);
 
