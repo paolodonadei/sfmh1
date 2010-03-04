c1852865b9593f39719d3152feb64bdd27b316ec
haach76 <haach76@bdf1b784-0ec1-11de-a4b8-0f3aab1a9b0e>
2010-03-04 22:50:35 +0000

    error is now in percentage

--- selfcalibmatlab/calcSelfCalibError.m
+++ selfcalibmatlab/calcSelfCalibError.m
@@ -1,4 +1,4 @@
- function [errf, errOC  ] = calcSelfCalibError(answerF,answerloc,ks)
+function [errf, errOC  ] = calcSelfCalibError(answerF,answerloc,ks)
 
 K1=ks{1,1};
 K2=ks{1,2};
@@ -11,32 +11,36 @@ trueX=0;
 trueY=0;
 for i=1:n
     trueF1=trueF1+abs(ks{1,i}(1,1));
-        trueF2=trueF2+abs(ks{1,i}(2,2));
-        
-        trueX= trueX+abs(ks{1,i}(1,3));
-        trueY=trueY+abs(ks{1,i}(2,3));
+    trueF2=trueF2+abs(ks{1,i}(2,2));
+
+    trueX= trueX+abs(ks{1,i}(1,3));
+    trueY=trueY+abs(ks{1,i}(2,3));
 end
 
 
 trueF1=trueF1/n;
 trueF2=trueF2/n;
 
- trueX= trueX/n;
- trueY=trueY/n;
-        
+trueX= trueX/n;
+trueY=trueY/n;
 
-f1err=abs(answerF(1,1)-trueF1);
-f2err=abs(answerF(1,2)-trueF2);
 
-xerror=abs(trueX-answerloc(1,1));
-yerror=abs(trueY-answerloc(1,2));
+f1err=(abs(answerF(1,1)-trueF1)/trueF1);
+f2err=(abs(answerF(1,2)-trueF2)/trueF2);
 
-errf=(f1err+f2err);
+xerror=(abs(trueX-answerloc(1,1))/trueX;
+yerror=(abs(trueY-answerloc(1,2))/trueY;
 
-errOC=xerror+yerror;
+errf=((f1err+f2err)/2)*100;
 
-if(errf>1000)
-    errf=1000;
+errOC=((xerror+yerror)/2)*100;
+
+if(errf>100)
+    errf=100;
+end
+
+if(errOC>100)
+    errOC=100;
 end
 
 end
