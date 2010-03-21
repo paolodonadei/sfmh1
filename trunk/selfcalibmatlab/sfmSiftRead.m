c5d85532bae430c877cc0a0dd5859368e1729868
haach76 <haach76@bdf1b784-0ec1-11de-a4b8-0f3aab1a9b0e>
2010-03-21 23:44:20 +0000

    misc

--- trunk/selfcalibmatlab/sfmSiftRead.m
+++ trunk/selfcalibmatlab/sfmSiftRead.m
@@ -24,8 +24,20 @@ FFiles=dir([realFold '*FMotion*']);
 
 for i=1:m
     
-   F{1,i}=load( [realFold FFiles(i,1).name]);
+    F{1,i}=load( [realFold FFiles(i,1).name]);
+    claim = 'This is a good example.';
+    new_name = strrep([realFold FFiles(i,1).name], 'FMotion', 'matches');
+    corrsmatrix= dlmread(new_name,'\t', 2, 1);
     
+    x1(1,:)=corrsmatrix(:,2)';
+    x1(2,:)=corrsmatrix(:,3)';
+    x1(3,:)=ones(1,size(x1,2));
+    
+    x2(1,:)=corrsmatrix(:,4)';
+    x2(2,:)=corrsmatrix(:,4)';
+    x2(3,:)=ones(1,size(x2,2));
+    
+    corrs{1,i} LEFT OFF HERE
 end
 
 
@@ -36,8 +48,8 @@ PFiles=dir([originalFold '*.P']);
 
 for i=1:m
     
-   P{1,i}=load( [originalFold PFiles(i,1).name]);
-   [ks{1,i}, R, t] = vgg_KR_from_P(P{1,i});
+    P{1,i}=load( [originalFold PFiles(i,1).name]);
+    [ks{1,i}, R, t] = vgg_KR_from_P(P{1,i});
     
 end
 
