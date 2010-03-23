f3ce5998482e950be92c7fa4f66f90772ce7a935
haach76 <haach76@bdf1b784-0ec1-11de-a4b8-0f3aab1a9b0e>
2010-03-23 23:04:59 +0000

    misc

--- trunk/selfcalibmatlab/generateFImages.m
+++ trunk/selfcalibmatlab/generateFImages.m
@@ -9,7 +9,7 @@ nonmaxrad = 3;  % Non-maximal suppression radius
 w = 7;    % Window size for correlation matching
 t = .001;  % Distance threshold for deciding outliers
 showgraphs=0;
-count=0;
+countF=0;
 
 
 
@@ -17,7 +17,7 @@ maxfunds=11;
 
 
 dirname='C:\Documents and Settings\hrast019\Desktop\data\euclidean\';
-dirname='/home/houman/work/test_data/';
+%dirname='/home/houman/work/test_data/';
 realFold=[dirname seqname '/'];
 imgformats={'pgm','jpg','png','tif'};
 numimages=0;
@@ -64,11 +64,11 @@ end
 for i=1:numIs
     for j=1:i
         if(i~=j)
-     
-          
-            count=count+1;
-            disp(['fundamental matrix number ' num2str(count) ' between frame ' num2str(i) ' with ' num2str(j)]);
-            clear cim1 r1 c1 cim2 r2 c2 m1 m2 F  inliers im1 im2 m1 m2 x1 x2 I1 I2 frames1 descriptors1 frames2 descriptors2;
+            
+            
+            countF=countF+1;
+            disp(['fundamental matrix number ' num2str(countF) ' between frame ' num2str(i) ' with ' num2str(j)]);
+            clear cim1 r1 c1 cim2 r2 c2 m1 m2 F   inliers im1 im2 m1 m2 x1 x2 I1 I2 frames1 descriptors1 frames2 descriptors2 xin1 xin2;
             
             [vm1,vn1,vj1]=size(IMS{1,i});
             [vm2,vn2,vj2]=size(IMS{1,j});
@@ -110,15 +110,30 @@ for i=1:numIs
                 x1=zeros(3,sn);
                 x2=zeros(3,sn);
                 
+                
+                
                 for p=1:sn
                     x1(1,p)= frames1(1,matches(1,p)) ;
                     x1(2,p)= frames1(2,matches(1,p)) ;
                     x1(3,p)=1  ;
+                    m1(1,p)= x1(2,p) ;
+                    m1(2,p)= x1(1,p) ;
                     
                     x2(1,p)=  frames2(1,matches(2,p));
                     x2(2,p)=  frames2(2,matches(2,p));
                     x2(3,p)= 1 ;
+                    m2(1,p)= x2(2,p) ;
+                    m2(2,p)= x2(1,p) ;
                 end
+                
+                for p=1:sn
+                    r1(p,1)= x1(2,p)  ;
+                    c1(p,1)= x1(1,p)  ;
+                    
+                    r2(p,1)= x2(2,p)  ;
+                    c2(p,1)= x2(1,p)  ;
+                end
+                
                 
                 %         plotmatches(I1,I2,frames1(1:2,:),frames2(1:2,:),matches) ;
                 %         drawnow ;
@@ -126,22 +141,41 @@ for i=1:numIs
             
             
             [F, inliers] = ransacfitfundmatrix(x1, x2, t);
-            corrs{1,count}=x1;
-            corrs{2,count}=x2;
+            MF{countF}=F;
+            
+            [im,in]=size(inliers);
+            
+         
+            for k=1:in
+                xin1(:,k)= x1(:,inliers(1,k));
+                xin2(:,k)= x2(:,inliers(1,k));
+               
+            end
+            corrs{1,countF}=xin1;
+            corrs{2,countF}=xin2;
             
             
             l2 = F*x1;    % Epipolar lines in image2
             l1 = F'*x2;   % Epipolar lines in image1
             
             if(showgraphs==1)
+                
                 show(im1,1), hold on, plot(c1,r1,'r+');
+                saveas(gcf,[seqname  '_features_im' num2str(i) '.png']);
+                saveas(gcf,[seqname  '_features_im' num2str(i) '.eps'],'epsc');
+                
                 show(im2,2), hold on, plot(c2,r2,'r+');
+                saveas(gcf,[seqname  '_features_im' num2str(j) '.png']);
+                saveas(gcf,[seqname  '_features_im' num2str(j) '.eps'],'epsc');
                 
                 % Display putative matches
                 show(im1,3), set(3,'name','Putative matches'), hold on
                 for n = 1:length(m1);
                     line([m1(2,n) m2(2,n)], [m1(1,n) m2(1,n)])
                 end
+                saveas(gcf,[seqname  '_putative_' num2str(i) 'to_' num2str(j) '.png']);
+                saveas(gcf,[seqname  '_putative_' num2str(i) 'to_' num2str(j) '.eps'],'epsc');
+                
                 % Display both images overlayed with inlying matched feature points
                 show(double(im1)+double(im2),4), set(4,'name','Inlying matches'), hold on
                 plot(m1(2,inliers),m1(1,inliers),'r+');
@@ -150,26 +184,29 @@ for i=1:numIs
                 for n = inliers
                     line([m1(2,n) m2(2,n)], [m1(1,n) m2(1,n)],'color',[0 0 1])
                 end
+                saveas(gcf,[seqname  '_inlier_' num2str(i) 'to_' num2str(j) '.png']);
+                saveas(gcf,[seqname  '_inlier_' num2str(i) 'to_' num2str(j) '.eps'],'epsc');
                 
-                for n = inliers
-                    figure(1), clf, show(im1,1), hold on, plot(x1(1,n),x1(2,n),'r+');
-                    hline(l1(:,n));
-                    figure(2), clf, show(im2,2), hold on, plot(x2(1,n),x2(2,n),'r+');
-                    hline(l2(:,n));
-                    fprintf('hit any key to see next point\r'); pause
-                end
+                %                 for n = inliers
+                %                     figure(1), clf, show(im1,1), hold on, plot(x1(1,n),x1(2,n),'r+');
+                %                     hline(l1(:,n));
+                %                     figure(2), clf, show(im2,2), hold on, plot(x2(1,n),x2(2,n),'r+');
+                %                     hline(l2(:,n));
+                %                     fprintf('hit any key to see next point\r'); pause
+                %                 end
                 
                 
-                fprintf('hit any key to see next point\r'); pause
+                
+                close all;
+                
+                
+                
+                
+                
+                
             end
-            
-            
-            
-            
-            MF{count}=F;
         end
     end
-end
-
-toc
+    
+    toc
 end
