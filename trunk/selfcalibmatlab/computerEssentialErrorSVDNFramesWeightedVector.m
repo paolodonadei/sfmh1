21eb716b8aef42645530b7f10ba3c575ddd79963
haach76 <haach76@bdf1b784-0ec1-11de-a4b8-0f3aab1a9b0e>
2010-03-24 23:07:57 +0000

    misc

---
+++ trunk/selfcalibmatlab/computerEssentialErrorSVDNFramesWeightedVector.m
@@ -0,0 +1,46 @@
+function [F] = computerEssentialErrorSVDNFramesWeightedVector(x,MYF,W)
+%this function , given a camera center and a focal length and a fundamental
+%matrix computes the error with respect to a fundamental matrix
+
+[m,n]=size(MYF);
+
+
+if (nargin == 2)
+   W=ones(n,1);
+end
+
+[l,k]=size(W);
+
+if(k~=m || n~=l)
+    disp(['the weights are not the right size, in computerEssentialErrorSVDNFramesWeighted']);
+    MYF
+    W
+end
+
+
+F=0;
+for i=1:n
+
+    T = computerEssentialErrorSVD(x,MYF{1,i});
+
+    F(i)=(T*W(i,1));
+   
+end
+
+sumW=sum(W);
+
+if( sumW>eps)
+    F=F/sumW; %make sure the sum of your weights is constant
+else
+    F=100;
+    disp('sum of W is zero inside computerEssentialErrorSVDNFramesWeighted, this shouldnt happen');
+end
+% 
+% D=W';
+% Z
+% D
+% c = D.*Z;
+% c
+
+end
+
