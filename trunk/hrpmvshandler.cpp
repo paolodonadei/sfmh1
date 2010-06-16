9088f008e02b36de4d725e6d94a8134aee9edb4c
haach76 <haach76@bdf1b784-0ec1-11de-a4b8-0f3aab1a9b0e>
2010-06-16 23:59:48 +0000

    adding stuff for pmvs handling, i should add the copy right for fukunaga here

--- trunk/hrpmvshandler.cpp
+++ trunk/hrpmvshandler.cpp
@@ -1,11 +1,80 @@
+#include <iostream>
+#include "boost/filesystem.hpp"   // includes all needed Boost.Filesystem declarations
+#include "boost/filesystem/operations.hpp"
+#include "boost/filesystem/path.hpp"
+
 #include "hrpmvshandler.h"
 
-HRpmvshandler::HRpmvshandler(string tempFolder, string origFolder)
+#if defined(_WIN32) || defined(WIN32) || defined(__CYGWIN__) || defined(__MINGW32__) || defined(__BORLANDC__)
+#define OS_WIN
+#endif
+
+using namespace std;
+
+const string linuxExecName="PMVS2.exe";
+const string windowsExecName="pmvs2";
+const string modelsFolder="models";
+const string imageFolder="visualize";
+const string projectionFolder="txt";
+const string optionFilename="option.txt";
+
+HRpmvshandler::HRpmvshandler(string tempFolder, string origFolder , HRImageSet* pimSet)
 {
-    //ctor
+    printf("input folder %s output folder %s \n", origFolder.c_str() ,tempFolder.c_str() );
+    SettempFOLD(tempFolder );
+    SetorigFOLD(origFolder);
+     SetimSet(pimSet);
 }
 
 HRpmvshandler::~HRpmvshandler()
 {
     //dtor
+}
+void HRpmvshandler::runPMVS()
+{
+    createPMVSFolder();
+    copyImagesToPMVSFolder();
+copyProjectionsToPMVSFolder();
+
+#ifdef OS_WIN
+   runPMVSWindows();
+#else
+   runPMVSLinux();
+#endif
+writeOptionFile();
+
+cleanUpPMVS();
+}
+
+
+
+
+void HRpmvshandler::createPMVSFolder()
+{
+
+}
+void HRpmvshandler::copyImagesToPMVSFolder()
+{
+
+}
+void HRpmvshandler::copyProjectionsToPMVSFolder()
+{
+
+}
+
+void HRpmvshandler::runPMVSLinux()
+{
+
+}
+void HRpmvshandler::runPMVSWindows()
+{
+
+}
+void HRpmvshandler::cleanUpPMVS()
+{
+
+}
+void HRpmvshandler::writeOptionFile()
+{
+
 }
--- trunk/hrpmvshandler.h
+++ trunk/hrpmvshandler.h
@@ -1,12 +1,19 @@
 #ifndef HRPMVSHANDLER_H
 #define HRPMVSHANDLER_H
 
+#include <sstream>
+#include <string>
+#include <vector>
+#include <typeinfo>
+#include "HRImage.hpp"
+
+using namespace std;
 
 class HRpmvshandler
 {
     public:
         /** Default constructor */
-        HRpmvshandler(string tempFolder, string origFolder);
+       HRpmvshandler(string tempFolder, string origFolder , HRImageSet* pimSet);
         /** Default destructor */
         virtual ~HRpmvshandler();
         /** Access tempFOLD
@@ -25,10 +32,22 @@ class HRpmvshandler
          * \param val New value to set
          */
         void SetorigFOLD(string val) { origFOLD = val; }
+  void SetimSet(HRImageSet* pSet) { mimSet = pSet; }
+
+        void createPMVSFolder();
+        void copyImagesToPMVSFolder();
+        void copyProjectionsToPMVSFolder();
+
+         void runPMVSLinux();
+         void runPMVSWindows();
+         void cleanUpPMVS();
+            void runPMVS();
+                 void writeOptionFile();
     protected:
     private:
         string tempFOLD; //!< Member variable "tempFOLD"
         string origFOLD; //!< Member variable "origFOLD"
+         HRImageSet* mimSet;
 };
 
 #endif // HRPMVSHANDLER_H
