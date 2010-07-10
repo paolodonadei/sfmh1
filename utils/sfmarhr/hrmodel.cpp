8426a9f5cf09a847a5f2ed629bd792a94b046498
haach76 <haach76@bdf1b784-0ec1-11de-a4b8-0f3aab1a9b0e>
2010-07-10 00:31:45 +0000

    models load now'

---
+++ utils/sfmarhr/hrmodel.cpp
@@ -0,0 +1,43 @@
+#include "hrmodel.h"
+#include "glm.h"
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+HRModel::HRModel(string fname)
+{
+    filename=fname;
+    model=NULL;
+}
+
+HRModel::~HRModel()
+{
+    //dtor
+}
+void HRModel::openOBJ()
+{
+
+
+    model=glmReadOBJ(filename.c_str());
+    if (!model)
+    {
+        printf("\nUsage: objviewV2 <-s> <obj filename>\n");
+        exit(0);
+    }
+
+     glmUnitize(model);
+
+
+    glmFacetNormals(model);
+
+    glmVertexNormals(model, 90.0, GL_TRUE);
+
+
+    printf("number of triangles is %d \n",model->groups->numtriangles);
+
+}
+void HRModel::drawOBJ()
+{
+       glPushMatrix();
+ glmDraw(model, GLM_SMOOTH);
+glPopMatrix();
+}
