847e7e4f8af85b44f0e999e1165b847d32cc6131
haach76 <haach76@bdf1b784-0ec1-11de-a4b8-0f3aab1a9b0e>
2010-10-13 03:01:43 +0000

    isc

--- selfcalibmatlab/PeterSturmSelfRobust.m
+++ selfcalibmatlab/PeterSturmSelfRobust.m
@@ -15,18 +15,22 @@ typicalF=5000;
 
 numFs=n;
 
-solvector=zeros(numFs,1);
+%solvector=zeros(numFs,1);
 
+curcounter=1;
 for i=1:numFs
     clear G U S V x a b ;
-
-
+    
+    if(isempty(F{1,i}))
+        continue;
+    end
+    
     G=    normalizeFSturm(F{1,i},w,h,typicalF);
-
-
-
+    
+    
+    
     flagerr=0;
-
+    
     try
         [U,S,V] = svd(G);
     catch
@@ -37,83 +41,84 @@ for i=1:numFs
         V=zeros(3,3);
         flagerr=1;
     end
-
+    
     if(flagerr==0)
-
+        
         a=S(1,1);
         b=S(2,2);
-
-
+        
+        
         U31=U(3,1);
         U32=U(3,2);
-
+        
         V31=V(3,1);
         V32=V(3,2);
         %quadratic:
-
+        
         c3=a ^ 2 * (1 - U31 ^ 2) * (1 - V31 ^ 2) - b ^ 2 * (1 - U32 ^ 2) * (1 - V32 ^ 2);
         c2= a ^ 2 * (U31 ^ 2 + V31 ^ 2 - 2 * U31 ^ 2 * V31 ^ 2) - b ^ 2 * (U32 ^ 2 + V32 ^ 2 - 2 * U32 ^ 2 * V32 ^ 2);
         c1=a ^ 2 * U31 ^ 2 * V31 ^ 2 - b ^ 2 * U32 ^ 2 * V32 ^ 2;
-
+        
         r=[c3 c2 c1];
-
-
+        
+        
         s = roots(r);
-
+        
         [ms,ns]=size(s);
-
+        
         if(ms~=0)
-
+            
             r1=s(1,1);
             r2=s(2,1);
         else
             r1=0;
             r2=0;
         end
-
+        
         f1=0;
         f2=0;
-
+        
         if(r1>0)
             fq1=sqrt(r1)*typicalF;
             f1=fq1;
             f2=fq1;
-
+            
         end
-
-
+        
+        
         if(r2>0)
             fq2=sqrt(r2)*typicalF;
             f1=fq2;
             f2=fq2;
         end
-
+        
         %linear 1
         C1=(-U32 * V31 * (a * U31 * V31 + b * U32 * V32) / (a * U31 * U32 * (1 - V31 ^ 2) + b * V31 * V32 * (1 - U32 ^ 2)));
-
+        
         if(C1>0)
-
+            
             FL1 = sqrt(C1)*typicalF;
-
+            
         end
-
+        
         %linear 2
         C2=-U31 * V32 * (a * U31 * V31 + b * U32 * V32) / (a * V31 * V32 * (1 - U31 ^ 2) + b * U31 * U32 * (1 - V32 ^ 2));
-
-
+        
+        
         if(C2>0)
-
+            
             FL2 = sqrt(C2)*typicalF;
-
+            
         end
-
+        
         x=[f1 f2];
-
+        
     else
         x=[0 0];
     end
-
-    solvector(i,1)=x(1,1);
+    
+    solvector(curcounter,1)=x(1,1);
+    curcounter=curcounter+1;
 end
 
 sln=mean(solvector);
---
+++ selfcalibmatlab/S3ObjectiveRotationDist.m
@@ -0,0 +1,84 @@
+function x = S3ObjectiveRotationDist(p, data)
+
+nowKs = convertLinetoMats(p,data{2,1}, data{3,1},data{4,1},data{5,1},data{6,1});
+
+
+x=0;
+count=0;
+
+for i=1:data{4,1}
+    for j=1:i
+        if(i~=j)
+            
+            count=count+1;
+             ersvd(count)=0;
+            ersvd(count)=errorSingleF(nowKs{j},nowKs{i},data{1,1}{i,j});
+            x=x+ ersvd(count);
+        end
+    end
+end
+
+count=0;
+y=0;
+for i=1:data{4,1}
+    for j=1:data{4,1}
+        for k=1:data{4,1}
+            if(i~=j && j~=k && i~=k)
+                
+                count=count+1;
+                erdist(count)=0;
+
+                erdist(count)=errorthreeFdist(nowKs{i},nowKs{j},nowKs{k},data{1,1}{i,j},data{1,1}{j,k},data{1,1}{i,k},reshape(data{8,1}{i,j}(:,1),3,2),reshape(data{8,1}{j,k}(:,1),3,2),reshape(data{8,1}{i,k}(:,1),3,2));
+                y=y+ erdist(count);
+            end
+        end
+    end
+end
+
+
+[m,n]=size(p);
+x=(ones(m,n)*x*1000000.0)/count;
+
+x;
+end
+function x = errorthreeFdist(Ki,Kj,Kk,Fij,Fjk,Fik,Xij,Xjk,Xik)
+
+Eij=(Kj')*Fij*Ki;
+Ejk=(Kk')*Fjk*Kj;
+Eik=(Kk')*Fik*Ki;
+
+[PijC] = getCameraMatrix(Eij);
+[PjkC] = getCameraMatrix(Ejk);
+[PikC] = getCameraMatrix(Eik);
+
+[Pij] = getCorrectCameraMatrix(PijC, Ki,Kj, Xij);
+[Pjk] = getCorrectCameraMatrix(PjkC, Kj,Kk, Xjk);
+[Pik] = getCorrectCameraMatrix(PikC, Ki,Kk, Xik);
+
+[KOij, Rij, tij] = vgg_KR_from_P(Pij);
+[KOjk, Rjk, tjk] = vgg_KR_from_P(Pjk);
+[KOik, Rik, tik] = vgg_KR_from_P(Pik);
+
+x=0;
+end
+function x = errorSingleF(K1,K2,F)
+
+
+G=(K1')*F*K2;
+
+try
+    S = svd(G);
+catch
+    S= [ 100 ;  10 ;  5];
+end
+
+if( S(2,1)>eps)
+    x=(((S(1,1)-S(2,1))/S(2,1))); % should not be squared but all my thresholds are based on this being squared
+    
+else
+    
+    x=100;
+end
+
+
+end
