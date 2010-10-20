007ae4df2644e5bb74a49b9d7f5a7ca4edc12a95
haach76 <haach76@bdf1b784-0ec1-11de-a4b8-0f3aab1a9b0e>
2010-10-20 02:16:19 +0000

    changes and now rotation si working

--- trunk/selfcalibmatlab/findERotDiff.m
+++ trunk/selfcalibmatlab/findERotDiff.m
@@ -1,12 +1,17 @@
 function [RP12,RP23,RP13, q12, q23, q13 ]= findERotDiff(R1,R2,R3)
 
 RP12=R2*inv(R1);
-RP23=R3*inv(R1);
-RP13=R3*inv(R2);
+RP23=R3*inv(R2);
+RP13=R3*inv(R1);
 
 [q12] = rotmat2quat(RP12);
 [q23] = rotmat2quat(RP23);
 [q13] = rotmat2quat(RP13);
+
+RRes= inv(RP13)*RP23*RP12
+
+
+QRes=  quatmultiply(quatconj(q13),quatmultiply(q23,q12 ) )
 
 
 end
