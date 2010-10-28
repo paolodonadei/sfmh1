64c4f570152e1f6996984db14804abe2e95a9bf0
haach76 <haach76@bdf1b784-0ec1-11de-a4b8-0f3aab1a9b0e>
2010-10-28 01:46:05 +0000

    misc

---
+++ trunk/selfcalibmatlab/findError3angle.m
@@ -0,0 +1,16 @@
+function [x] = findError3angle(ij, jk, ik)
+
+%normalize
+ij=ij/norm(ij);
+jk=jk/norm(jk);
+ik=ik/norm(ik);
+
+a_ij_ik=acos(dot(ij,ik));
+a_ji_jk=acos(dot(-ij,jk));
+a_ki_kj=acos(dot(-ik,-jk));
+
+total=(180-(((a_ij_ik+ a_ji_jk+ a_ki_kj)*180)/pi))/180;
+
+x=total;
+
+end
