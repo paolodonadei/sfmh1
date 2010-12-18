008c7e0e17f79d845f4c7a8b8dcb41e457fb980f
haach76 <haach76@bdf1b784-0ec1-11de-a4b8-0f3aab1a9b0e>
2010-12-18 02:16:50 +0000

    misc

--- trunk/selfcalibmatlab/generateCorrsforF.m
+++ trunk/selfcalibmatlab/generateCorrsforF.m
@@ -1,5 +1,8 @@
 function [ Fgt,k1,k2,corrs, I1, I2, I1motion, R1, t1,R2,t2 ] = generateCorrsforF(numcorrs, outlierratio, stderror, type, seqname )
-
+%this function generates a virtual or a real pair of images with the given
+%amount of outliers and inliers with the given amount of gaussian noise
+%added to everything and also the seqname is either the sequence name or
+%the directory where the images reside
 if nargin < 5
       seqname='merton1';
 end
@@ -14,10 +17,15 @@ if(type=='s')
     [status currdir] = system(comma );
     M = dlmread('matches.csv',',',1);
     
-    I1=imread('correspondences_drprevimg.png');
-    I2=imread('correspondences_drnextimg.png');
-    I1motion=imread('correspondences_dr.png');
-    k1=load('intrinsic_synth_data.txt');
+%    I1=imread('correspondences_drprevimg.png');
+%    I2=imread('correspondences_drnextimg.png');
+%    I1motion=imread('correspondences_dr.png');
+    I1=zeros(512,512);
+    I2=zeros(512,512);
+    
+  
+
+k1=load('intrinsic_synth_data.txt');
     k2=load('intrinsic_synth_data2.txt');
     Fgt=load('trueFmatrix_synth_data.txt');
     t1=zeros(3,1);
@@ -33,29 +41,33 @@ end
 
 %for sift
 if(type=='f')
-    oldFolder = cd('../utils/CRC2D/');
-    partonecom='./crc2Dto3D ';
-    partTwo=[' -N ' num2str(numcorrs) ' -E ' num2str(outlierratio) ' -v ' num2str(stderror) ' -T 3 -q 1'];
-    comma=[partonecom partTwo];
-    [status currdir] = system(comma );
-    M = dlmread('matches.csv',',',1);
     
-    I1=imread('correspondences_drprevimg.png');
-    I2=imread('correspondences_drnextimg.png');
-    I1motion=imread('correspondences_dr.png');
-    k1=load('intrinsic_synth_data.txt');
-    k2=load('intrinsic_synth_data2.txt');
-    Fgt=load('trueFmatrix_synth_data.txt');
-    R=load('rotation_synth_data.txt');
-    t=load('trans_synth_data.txt');
-    x1=M(:,2:3)';
-    x2=M(:,4:5)';
-    status=M(:,6)';
-    corrs=[x1 ; x2];
-    cd(oldFolder);
+    % so here we assume seqname is the directory name and we read the first
+    % two images
+    
+      if(exist(seq_name,'dir')==0)
+        display(['directory can not be found']);
+      end
+    
+      imdirs=dir([seq_name '*.jpg']);
+ [ corrs, I1, I2 ] = SIFTmatchpair(fname1,fname2 );
+ R1=eye(3);
+ R2=eye(3);
+ t1=zeros(3,1);
+ t2=zeros(3,1);
 end
 
 
+%for oxford
+if(type=='o')
+ [corrs, IMS, P,K, F, E, FFormatted, corrsFormatted,EFormatted,FFormattedGT] = readCorrsOxford(seqname, outlierratio, 1,2,stderror);
+ corrs=corrsFormatted{1,2};
+ [k1, R1, t1] = vgg_KR_from_P(P{1});
+ [k2, R2, t2] = vgg_KR_from_P(P{2});
+ I1=IMS{1};
+ I2=IMS{2};
+ Fgt=FFormattedGT{1,2};
+end
 
 
 
