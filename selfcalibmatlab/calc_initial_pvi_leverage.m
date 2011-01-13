112726d2fb8cfb83975f4d39dfdf70b68e542bf1
haach76 <haach76@bdf1b784-0ec1-11de-a4b8-0f3aab1a9b0e>
2011-01-13 02:12:44 +0000

    misc"

---
+++ selfcalibmatlab/calc_initial_pvi_leverage.m
@@ -0,0 +1,43 @@
+function [pvi] = calc_initial_pvi_leverage(x1, x2)
+
+
+
+if nargin == 1
+    corrs=x1;
+    x1=corrs(1:2,:);
+    x2=corrs(3:4,:);
+end
+
+[m1,n1]=size(x1);
+[m2,n2]=size(x1);
+
+if(m1~=m2 || n1~=n2)
+    display('size of data is not right');
+end
+
+if(m1==2)
+    x1=[x1 ; ones(1,n1)];
+    x2=[x2 ; ones(1,n1)];
+end
+
+[x1n, T1] = normalise2dpts(x1);
+[x2n, T2] = normalise2dpts(x2);
+
+[m,npts]=size(x1);
+% Build the constraint matrix
+A = [x2n(1,:)'.*x1n(1,:)'   x2n(1,:)'.*x1n(2,:)'  x2n(1,:)' ...
+    x2n(2,:)'.*x1n(1,:)'   x2n(2,:)'.*x1n(2,:)'  x2n(2,:)' ...
+    x1n(1,:)'             x1n(2,:)'            ones(npts,1) ];
+
+h = leverage(A);
+
+
+minh=min(h);
+maxh=max(h);
+rangeh=maxh-minh;
+hn=(h-minh)*(1/rangeh);
+
+pvi=exp(-(hn*5));
+
+
+end
