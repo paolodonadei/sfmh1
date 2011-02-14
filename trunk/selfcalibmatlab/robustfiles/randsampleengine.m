6634b89f913ff7de1b62b049a49cfb2731b4d36a
haach76 <haach76@bdf1b784-0ec1-11de-a4b8-0f3aab1a9b0e>
2011-02-14 06:04:18 +0000

    added the liang method to the matlab files

--- trunk/selfcalibmatlab/robustfiles/randsampleengine.m
+++ trunk/selfcalibmatlab/robustfiles/randsampleengine.m
@@ -1,7 +1,7 @@
 function z=randsampleengine(s, n, pvi,typenum)
 if(typenum==1 || typenum==2)
     z= randsample(s, n);
-elseif(typenum==3 || typenum==4 || typenum==5)
-    y = monteCarloSampling(s, n, pvi);
+elseif(typenum==3 || typenum==4 || typenum==5 || typenum==6)
+    z = monteCarloSampling(s, n, pvi);
 end
 end
