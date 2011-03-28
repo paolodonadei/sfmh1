29bad6c1a039fb626b740f3767f60d79c19e29bb
haach76 <haach76@bdf1b784-0ec1-11de-a4b8-0f3aab1a9b0e>
2011-03-28 00:54:39 +0000

    test

--- selfcalibmatlab/pvifitness.m
+++ selfcalibmatlab/pvifitness.m
@@ -20,28 +20,28 @@ errorin=sum(errors(find(inliers==1)))/(eps+sum(inliers));
 errorout=sum(errors(find(inliers==0)))/(m-sum(inliers)+eps);
 errorstot=mean( errors);
 
-% display(['mean of inlier pvis is: ' num2str(errorin)]);
-% display(['mean of outlier pvis is: ' num2str(errorout)]);
-% display(['mean of pvis is: ' num2str(errorstot)]);
-% % % 
-% display(['mean error of inliers is : ' num2str(errorin)]);
-% display(['mean error of outliers is : ' num2str(errorout)]);
-% display(['**total error is : ' num2str((errorin+errorout)/2)]);
-% 
-% subplot(2,2,1)
-% hist(pvis,100);
-% title([ ' histogram of pvis']);
-% subplot(2,2,2)
-% hist(errors,100);
-% title([ ' histogram of pvi errors']);
-% subplot(2,2,3)
-% 
-% hist(pvis(find(inliers==1)),100);
-% title([ ' histogram of inlier pvis']);
-% subplot(2,2,4)
-% 
-% hist(pvis(find(inliers==0)),100);
-% title([ ' histogram of outlier pvis']);
+display(['mean of inlier pvis is: ' num2str(errorin)]);
+display(['mean of outlier pvis is: ' num2str(errorout)]);
+display(['mean of pvis is: ' num2str(errorstot)]);
+% % 
+display(['mean error of inliers is : ' num2str(errorin)]);
+display(['mean error of outliers is : ' num2str(errorout)]);
+display(['**total error is : ' num2str((errorin+errorout)/2)]);
+
+subplot(2,2,1)
+hist(pvis,100);
+title([ ' histogram of pvis']);
+subplot(2,2,2)
+hist(errors,100);
+title([ ' histogram of pvi errors']);
+subplot(2,2,3)
+
+hist(pvis(find(inliers==1)),100);
+title([ ' histogram of inlier pvis']);
+subplot(2,2,4)
+
+hist(pvis(find(inliers==0)),100);
+title([ ' histogram of outlier pvis']);
 
 
 end
